31becd686865b8fe1349e40cbde09253959291c2
TymonJones <101831045+Tjonte03@users.noreply.github.com>
2023-04-09 19:46:23 -0700

    Loop lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// for (let i = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+function writeCards(names, event) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push("Thank you, " + names[i] + ", for the wonderful " + event + " gift!")
+  }
+  return messages;
+} 
+
+let countDown = function(i) {
+  console.log(i);
+  i > 0 && countDown(i - 1);
+}
+  countDown(10);
+
